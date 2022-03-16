f7a4edda1387edddf792087f8270cada113de8db
Adam <adamniemiec862@gmail.com>
2022-03-16 14:40:35 +0100

    Init Lab2

---
+++ Lab2_PokemonRestApp/src/main/java/com/agh/lab2_pokemonrestapp/Lab2PokemonRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.agh.lab2_pokemonrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2PokemonRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2PokemonRestAppApplication.class, args);
+    }
+
+}
