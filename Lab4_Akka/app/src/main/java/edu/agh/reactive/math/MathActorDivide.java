0823016d73051213ea47edba969798a22360e323
Adam <adamniemiec862@gmail.com>
2022-04-07 12:49:37 +0200

    Implement MathActorDivide

--- Lab4_Akka/app/src/main/java/edu/agh/reactive/math/MathActorDivide.java
+++ Lab4_Akka/app/src/main/java/edu/agh/reactive/math/MathActorDivide.java
@@ -1,4 +1,39 @@
 package edu.agh.reactive.math;
 
-class MathActorDivide {
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+public class MathActorDivide extends AbstractBehavior<MathActor.MathCommandDivide> {
+    public MathActorDivide(ActorContext<MathActor.MathCommandDivide> context) {
+        super(context);
+    }
+
+    public static Behavior<MathActor.MathCommandDivide> create() {
+        return Behaviors.setup(MathActorDivide::new);
+    }
+
+    // --- define message handlers
+    @Override
+    public Receive<MathActor.MathCommandDivide> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(MathActor.MathCommandDivide.class, this::onMathCommandDivide)
+                .build();
+    }
+
+    private Behavior<MathActor.MathCommandDivide> onMathCommandDivide(MathActor.MathCommandDivide mathCommandDivide) {
+        System.out.println("actorDivide: received command: divide");
+        int result;
+        try {
+            result = mathCommandDivide.firstNumber / mathCommandDivide.secondNumber;
+        } catch (ArithmeticException e) {
+            result = 0;
+        }
+        System.out.println("actorDivide: divide result = " + result);
+        System.out.println("actorDivide: sending response");
+        mathCommandDivide.replyTo.tell(new MathActor.MathCommandResult(result));
+        return this;
+    }
 }
